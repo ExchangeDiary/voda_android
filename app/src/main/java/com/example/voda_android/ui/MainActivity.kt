3c532952f494e174dc3d248925dbab3d6aaf64b4
이상은 <sangeun@mobmail.shinhan.com>
2022-03-01 21:17:02 +0900

    힐트 설정 추가

--- app/src/main/java/com/example/voda_android/ui/MainActivity.kt
+++ app/src/main/java/com/example/voda_android/ui/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.voda_android.ui
 import android.os.Bundle
 import com.example.voda_android.R
 import com.example.voda_android.ui.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
