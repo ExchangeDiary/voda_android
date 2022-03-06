70693d347f6510a01b86ab58b6ff8b47b434d0bb
sangeun <suev7245@gmail.com>
2022-03-06 16:57:12 +0900

    [#4- 네트워크 모듈 생성] 뷰에 버튼 생성하여 네트워크 모듈 정상 작동 확인 가능.

--- app/src/main/java/com/example/voda_android/ui/MainActivity.kt
+++ app/src/main/java/com/example/voda_android/ui/MainActivity.kt
@@ -1,14 +1,35 @@
 package com.example.voda_android.ui
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.viewModels
+import androidx.lifecycle.asLiveData
 import com.example.voda_android.R
 import com.example.voda_android.ui.base.BaseActivity
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.observeOn
 
 @AndroidEntryPoint
 class MainActivity : BaseActivity() {
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setClicks()
+        setObservers()
+    }
+
+    private fun setClicks() {
+        findViewById<Button>(R.id.button).setOnClickListener {
+            viewModel.getTest()
+        }
+    }
+
+    private fun setObservers(){
+        viewModel.testText.asLiveData().observe(this){
+            findViewById<TextView>(R.id.textView).text = it
+        }
     }
 }
